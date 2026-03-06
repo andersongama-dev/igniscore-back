cd9714d592f657ee5d1bc0ddf1de4d8ec16403cc
andersongama-dev <andersongamasilva5@gmail.com>
2026-03-06 01:46:05 -0300

    Create User service

---
+++ src/main/java/com/igniscore/api/service/UserService.java
@@ -0,0 +1,29 @@
+package com.igniscore.api.service;
+
+import com.igniscore.api.model.User;
+import com.igniscore.api.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User create(String name, String email, String password ) {
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        user.setPassword(password);
+
+        return repository.save(user);
+    }
+}
