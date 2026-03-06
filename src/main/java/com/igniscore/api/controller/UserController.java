55aa83cab109c8517b47e84f3c62aed6b586c631
andersongama-dev <andersongamasilva5@gmail.com>
2026-03-06 01:56:57 -0300

    Create User controller

---
+++ src/main/java/com/igniscore/api/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.igniscore.api.controller;
+
+import com.igniscore.api.model.User;
+import com.igniscore.api.service.UserService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    public List<User> users(){
+        return service.findAll();
+    }
+
+    @MutationMapping
+    public User createUser(@Argument String name,
+                           @Argument String email,
+                           @Argument String password) {
+        return service.create(name, email, password);
+    }
+
+}
